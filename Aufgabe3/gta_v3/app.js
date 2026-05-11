7098187aec810b394caa85f21ee12293ad8c12f8
unspecifiedz <162453463+unspecifiedz@users.noreply.github.com>
2026-05-11 12:54:07 +0000

    Task 2 Preparation

--- Aufgabe3/gta_v3/app.js
+++ Aufgabe3/gta_v3/app.js
@@ -43,6 +43,7 @@ app.use(express.urlencoded({ extended: false }));
  */
 
 // TODO: ... your code here ...
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set dedicated script for routing
 app.use('/', indexRouter);
